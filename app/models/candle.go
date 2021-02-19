4e462a833273b01b9e8f9148757d4ca1466baa4d
netooo <kazushi.mitsue@gmail.com>
2021-02-19 23:36:51 +0900

    Add: create candle

---
+++ app/models/candle.go
@@ -0,0 +1,43 @@
+package models
+
+import (
+	"fmt"
+	"time"
+)
+
+type Candle struct {
+	ProductCode string
+	Duration    time.Duration
+	Time        time.Time
+	Open        float64
+	Close       float64
+	High        float64
+	Low         float64
+	Volume      float64
+}
+
+func NewCandle(productCode string, duration time.Duration, timeDate time.Time, open, close, high, low, volume float64) *Candle {
+	return &Candle{
+		productCode,
+		duration,
+		timeDate,
+		open,
+		close,
+		high,
+		low,
+		volume,
+	}
+}
+
+func (c *Candle) TableName() string {
+	return GetCandleTableName(c.ProductCode, c.Duration)
+}
+
+func (c *Candle) Create() error {
+	cmd := fmt.Sprintf("INSERT INTO %s (time, open, close, high, low, volume) VALUES (?, ?, ?, ?, ?, ?)", c.TableName())
+	_, err := DbConnection.Exec(cmd, c.Time.Format(time.RFC3339), c.Open, c.Close, c.High, c.Low, c.Volume)
+	if err != nil {
+		return err
+	}
+	return err
+}
