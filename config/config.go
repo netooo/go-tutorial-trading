eacf5b00487533ccd7717eae9f4c059a0b3395f5
netooo <kazushi.mitsue@gmail.com>
2021-02-19 23:24:22 +0900

    Add: db scheme

--- config/config.go
+++ config/config.go
@@ -4,12 +4,20 @@ import (
 	"gopkg.in/ini.v1"
 	"log"
 	"os"
+	"time"
 )
 
 type ConfigList struct {
-	ApiKey    string
-	ApiSecret string
-	LogFile   string
+	ApiKey      string
+	ApiSecret   string
+	LogFile     string
+	ProductCode string
+
+	TradeDuration time.Duration
+	Durations     map[string]time.Duration
+	DbName        string
+	SQLDriver     string
+	Port          int
 }
 
 var Config ConfigList
@@ -21,9 +29,21 @@ func init() {
 		os.Exit(1)
 	}
 
+	durations := map[string]time.Duration{
+		"1s": time.Second,
+		"1m": time.Minute,
+		"1h": time.Hour,
+	}
+
 	Config = ConfigList{
-		ApiKey:    cfg.Section("api").Key("api_key").String(),
-		ApiSecret: cfg.Section("api").Key("api_secret").String(),
-		LogFile:   cfg.Section("trade").Key("log_file").String(),
+		ApiKey:        cfg.Section("api").Key("api_key").String(),
+		ApiSecret:     cfg.Section("api").Key("api_secret").String(),
+		LogFile:       cfg.Section("trade").Key("log_file").String(),
+		ProductCode:   cfg.Section("trade").Key("product_code").String(),
+		Durations:     durations,
+		TradeDuration: durations[cfg.Section("trade").Key("trade_duration").String()],
+		DbName:        cfg.Section("db").Key("name").String(),
+		SQLDriver:     cfg.Section("db").Key("driver").String(),
+		Port:          cfg.Section("web").Key("poet").MustInt(),
 	}
 }
--- main.go
+++ main.go
@@ -2,18 +2,12 @@ package main
 
 import (
 	"fmt"
-	"github.com/netooo/trade/bitflyer"
+	"github.com/netooo/trade/app/models"
 	"github.com/netooo/trade/config"
 	"github.com/netooo/trade/utils"
-	"time"
 )
 
 func main() {
 	utils.LoggingSettings(config.Config.LogFile)
-	apiClient := bitflyer.New(config.Config.ApiKey, config.Config.ApiSecret)
-	ticker, _ := apiClient.GetTicker("BTC_USD")
-	fmt.Println(ticker)
-	fmt.Println(ticker.GetMidPrice())
-	fmt.Println(ticker.DateTime())
-	fmt.Println(ticker.TruncateDateTime(time.Hour))
+	fmt.Println(models.DbConnection)
 }
