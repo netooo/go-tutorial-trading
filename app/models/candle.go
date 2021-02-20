32b75d74295e36180c6bd60165cafda2b4dedb43
netooo <kazushi.mitsue@gmail.com>
2021-02-20 18:20:42 +0900

    Add: create candle with duration

--- app/models/candle.go
+++ app/models/candle.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"fmt"
+	"github.com/netooo/trade/bitflyer"
 	"time"
 )
 
@@ -61,4 +62,24 @@ func GetCandle(productCode string, duration time.Duration, dateTime time.Time) *
 		return nil
 	}
 	return NewCandle(productCode, duration, candle.Time, candle.Open, candle.Close, candle.High, candle.Low, candle.Volume)
+}
+
+func CreateCandleWithDuration(ticker bitflyer.Ticker, productCode string, duration time.Duration) bool {
+	currentCandle := GetCandle(productCode, duration, ticker.TruncateDateTime(duration))
+	price := ticker.GetMidPrice()
+	if currentCandle == nil {
+		candle := NewCandle(productCode, duration, ticker.TruncateDateTime(duration), price, price, price, price, ticker.Volume)
+		_ = candle.Create()
+		return true
+	}
+
+	if currentCandle.High <= price {
+		currentCandle.High = price
+	} else if currentCandle.Low >= price {
+		currentCandle.Low = price
+	}
+	currentCandle.Volume += ticker.Volume
+	currentCandle.Close = price
+	currentCandle.Save()
+	return false
 }
